a117a829814758e4ce34d5a4e382d49478bfcb69
nkem-a <nkemanizoba@gmail.com>
2022-09-19 11:16:50 +0100

    Initial Commit

---
+++ OchommaFajas/OchommaFajasApp.swift
@@ -0,0 +1,17 @@
+//
+//  OchommaFajasApp.swift
+//  OchommaFajas
+//
+//  Created by user on 19/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct OchommaFajasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
